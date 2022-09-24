0956d633849094f3952b85ccb32fac682217f4fa
bkeener <101418582+bkeener7@users.noreply.github.com>
2022-09-24 12:53:17 -0600

    Revert "Feat/tests"

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,30 +1,5 @@
-#ruby ./lib/night_writer.rb message.txt braille.txt
-class NightWriter
+existing_file = ARGV[0]
+created_file = ARGV[1]
+character_length = File.read('../message.txt').length
 
-    def initialize 
-        @existing_filepath = ARGV[0]
-        @created_filepath = ARGV[1]
-    end
-
-    # def self.terminal_welcome
-    #     merge with startup message?
-    # end
-    
-    def character_length
-        File.read("#{created_filepath}").length
-    end
-
-    def startup_message
-        "Created '#{created_filepath}' containing #{character_length} characters"
-    end
-
-    def read_file(file_path)
-        File.read(file_path)
-    end
-
-    def write_file(writing_filepath, reading_filepath)
-        output_file = File.open(writing_filepath, 'w+')
-        output_file.write(read_file(reading_filepath))
-        output_file.close
-    end
-end
+puts "Created '#{ARGV[1]}' containing #{character_length} characters"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,4 @@
 require 'simplecov'
-require 'rspec'
-require './lib/night_writer'
-require './lib/braille'
-require './lib/dictionary'
 SimpleCov.start
 
-# #all require_relatives here
-require_relative 'night_writer_spec'
-require_relative 'braille_spec'
-require_relative 'dictionary_spec'
+#all require_relatives here
