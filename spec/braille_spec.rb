0e2ea3d7bb3d38b766a2eb1072a69093b6014368
bkeener <bkeener7@gmail.com>
2022-09-24 14:59:45 -0600

    Add wordwrap

--- spec/braille_spec.rb
+++ spec/braille_spec.rb
@@ -7,61 +7,17 @@ RSpec.describe Braille do
         expect(braille).to be_an_instance_of(Braille)
     end
 
-    # xit '2. holds an array for a 80 character line of braille' do
-    #     expected = [[[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]],
-    #     [[nil], [nil], [nil]]]
-    #     expect(braille.grid).to eq expected
-    # end
-
-    it '3. can print a single braille character' do
+    it '2. can print a single braille character' do
         allow(braille).to receive(:puts).and_return(double().as_null_object)
         expect(braille.print_character(:h)).to eq ([["0."], ["00"], [".."]])
     end
 
-    it '4. can add a braille character to the grid' do
+    it '3. can add a braille character to the grid' do
         braille.add_character(:h)
-        expect(braille.grid[0]).to eq([["0."], ["00"], [".."]])
+        expect(braille.grid).to eq([[["0."], ["00"], [".."]]])
     end
 
-    it '5. can print braille characters in correct vertical and horizontal order' do
+   it '4. can print braille characters in correct vertical and horizontal order' do
         allow(braille).to receive(:print).and_return(double().as_null_object)
         expected = [[["0."], ["00"], [".."]], [["0."], [".0"], [".."]], [["0."], ["0."], ["0."]], [["0."], ["0."], ["0."]], [["0."], [".0"], ["0."]]]
         expected_print = [["0.", "0.", "0.", "0.", "0."], ["00", ".0", "0.", "0.", ".0"], ["..", "..", "0.", "0.", "0."]]
@@ -75,4 +31,9 @@ RSpec.describe Braille do
         expect(braille.print_grid).to eq(expected_print)
     end
 
+    it '5. will only allow 40 braille characters in a grid' do
+        allow(braille).to receive(:print).and_return(double().as_null_object)
+        41.times { braille.add_character(:a) }
+        expect(braille.grid).to eq([[["0."], [".."], [".."]]])
+    end
 end
